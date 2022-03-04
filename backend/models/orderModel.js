fd20d197c5e07b042964e8798a6e55b2fa2d96bf
abhishekmulik <mulikabhishek2605@gmail.com>
2022-03-04 11:09:13 +0530

    model created

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,28 +1,76 @@
 import mongoose from "mongoose";
 
-const userSchema=mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const orderSchema=mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'User'
     },
-    email:{
+    orderItems:[
+        {
+            name:{type: String, required:true},
+            qty:{type: Number, required:true},
+            image:{type: String, required:true},
+            price:{type: Number, required:true},
+            product:{
+                type:mongoose.Schema.Types.ObjectId,
+                required:true,
+                ref:'Product'
+            },
+        }
+    ],
+    shippingAddress:{
+        address:{type:String,required:true},
+        city:{type:String,required:true},
+        postalCode:{type:String,required:true},
+        country:{type:String,required:true},
+    
+    },
+    paymentMethod:{
         type:String,
         required:true,
-        unique:true
     },
-    password:{
-        type:String,
-        required:true
+    paymentResult:{
+        id :{type:String},
+        status:{type:String},
+        update_item:{type:String},
+        email_address:{type:String}
     },
-    isAdmin:{
+    taxPrice:{
+        type:Number,
+        required:true,
+        default:0.0
+    },
+    shippingPrice:{
+        type:Number,
+        required:true,
+        default:0.0
+    },
+    totalPrice:{
+        type:Number,
+        required:true,
+        default:0.0
+    },
+    isPaid:{
         type:Boolean,
         required:true,
         default:false
+    },
+    paidAt:{
+        type:Date
+    },
+    isDelivered:{
+        type:Boolean,
+        required:true,
+        default:false
+    },
+    deliveredAt:{
+        type:Date
     }
 },{
     timestamps:true
 })
 
-const User=mongoose.Model('User',userSchema)
+const User=mongoose.Model('Order',orderSchema)
 
-export default User
+export default Order
