c4cae717fd97c5a31b942cecc7b0371d6b43c172
abhishekmulik <mulikabhishek2605@gmail.com>
2022-03-04 11:56:31 +0530

    seeder file added

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,6 @@
+
 const products = [
   {
-    _id: '1',
     name: 'Airpods Wireless Bluetooth Headphones',
     image: '/images/airpods.jpg',
     description:
@@ -13,24 +13,22 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: '2',
     name: 'iPhone 11 Pro 256GB Memory',
     image: '/images/phone.jpg',
     description:
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
-    brand: 'Apple',
-    category: 'Electronics',
+    'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+      brand: 'Apple',
+      category: 'Electronics',
     price: 599.99,
     countInStock: 7,
     rating: 4.0,
     numReviews: 8,
   },
   {
-    _id: '3',
     name: 'Cannon EOS 80D DSLR Camera',
     image: '/images/camera.jpg',
     description:
-      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
     brand: 'Cannon',
     category: 'Electronics',
     price: 929.99,
@@ -39,7 +37,7 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: '4',
+
     name: 'Sony Playstation 4 Pro White Version',
     image: '/images/playstation.jpg',
     description:
@@ -52,7 +50,7 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: '5',
+    
     name: 'Logitech G-Series Gaming Mouse',
     image: '/images/mouse.jpg',
     description:
@@ -65,7 +63,7 @@ const products = [
     numReviews: 10,
   },
   {
-    _id: '6',
+    
     name: 'Amazon Echo Dot 3rd Generation',
     image: '/images/alexa.jpg',
     description:
---
+++ backend/data/users.js
@@ -0,0 +1,26 @@
+import bcrypt from "bcryptjs"
+
+const users=[
+    {
+        name:'Admin User',
+        email:'admin@example.com',
+        password:bcrypt.hashSync('123456',10),
+        isAdmin:true
+    },
+    {
+        name:'John Doe',
+        email:'john@example.com',
+        password:bcrypt.hashSync('123456',10),
+        password:xxx,
+       
+    },
+    {
+        name:'Jane doe',
+        email:'Jane@example.com',
+        password:bcrypt.hashSync('123456',10),
+        password:xxx,
+       
+    }
+]
+
+export default users
