6c27ae2c89c3b5255be3db07c3a8a7f2f6e7d0c0
abhishekmulik <mulikabhishek2605@gmail.com>
2022-03-12 08:25:54 +0530

    product details reducers

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,34 @@
-import React,{ useState,useEffect } from 'react';
+import React,{ useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {Row,Col} from 'react-bootstrap'
 import axios from 'axios'
 import Product from '../components/Product';
-
+import { listProduct } from '../actions/productAction'
+import Loader from '../components/loader';
+import Message from '../components/message';
 
 function HomeScreen() {
-    const [products,setProducts]=useState([])
+    const dispatch=useDispatch()
+
+    const productList=useSelector( state =>state.productList )
+    const {loading,error,products}=productList
 
     useEffect(()=>{
-        const fetchProducts=async ()=>{
-            const {data}=await axios.get('/api/products')
-            setProducts(data)
-        }
-        fetchProducts()
-    },[])
-  return (<>
+        dispatch(listProduct())
+    },[dispatch])
+
+    
+  return (
+  <>
         <h1>Latest product</h1>
-        <Row>
+        {loading ? <Loader/>: error? <Message variant='danger'>{error}</Message>:  (<Row>
             {products.map(product=>(
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product}/>
                 </Col>
             ))}
-        </Row>
+        </Row>) }
+       
   </>);
 }
 
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,25 +2,31 @@ import React,{useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { Row,Col,Image,ListGroup, Card,Button, ListGroupItem } from 'react-bootstrap'
 import Ratings from '../components/Ratings'
-import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import {listProductDetails} from '../actions/productAction'
+import Loader from '../components/loader'
+import Message from '../components/message'
+
 
 function ProductScreen() {
-  const matches=useParams()
+  const match=useParams()
+  const dispatch=useDispatch()
 
-  const [product,setProduct]=useState({})
+  const productDetails=useSelector(state=>state.productDetails)
+  const { product, error,loading }= productDetails
+  
   useEffect(()=>{
-    const fetchProduct=async ()=>{
-      const { data } = await axios.get(`/api/products/${matches.id}`)
-      setProduct(data)
-    }
-    fetchProduct()
-  },[])
+    dispatch(listProductDetails(match.id))
+  },[dispatch,match])
 
+  
   return (
     <>
     <Link className='btn btn-dark my-3' to='/'>Go Back</Link>
-   <Row>
+    {
+      loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message>:
+      <Row>
      <Col md={6}>
        <Image src={product.image} alt={product.name} fluid/>
      </Col>
@@ -70,6 +76,9 @@ function ProductScreen() {
      </Col>
    </Row>
 
+
+    }
+   
 
    </>
   )
